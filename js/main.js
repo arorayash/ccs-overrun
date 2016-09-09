eb2484139e0976fb84ff86b579605d3dd57542ad
Yash Arora <arorayash.thisisus@gmail.com>
2016-09-09 19:23:07 +0530

    alpha commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+head.js(
+    { jquery : "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" },
+    { mousewheel : "js/jquery.mousewheel.js" },
+    { mwheelIntent : "js/mwheelIntent.js" },
+    { jScrollPane : "js/jquery.jscrollpane.min.js" },
+    { history : "js/jquery.history.js" },
+    { stringLib : "js/core.string.js" },
+    { easing : "js/jquery.easing.1.3.js" },
+    { smartresize : "js/jquery.smartresize.js" },
+    { page : "js/jquery.page.js" }
+);
+
